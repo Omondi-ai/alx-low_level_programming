8da32563ccd7334934576943f3fca8aa4e6bce97
Omondi-ai <odhiamboderrick01@gmail.com>
2022-07-03 03:26:35 -0700

    Printing them 3 combs

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+/**
+ * main - Prints 3 combinations of numbers
+ *
+ * Return: Always (Success)
+ */
+int main(void)
+{
+	int b, i, k, j;
+
+	for (b = 48; b <= 57; b++)
+	{
+		for (i = 48; i <= 57; i++)
+		{
+			for (k = 48; j <= 57; j++)
+			{
+				if (((k + j) > (c + i) && k >= b) || b < k)
+				{
+					putchar(b);
+					putchar(i);
+					putchar(' ');
+					putchar(k);
+					putchar(j);
+
+				if (b + i + k + j == 227 && c == 57)
+				{
+				break;
+				}
+				else
+				{
+				putchar(',');
+				putchar(' ');
+				}
+				}
+			}
+		}
+	}
+}
+
+putchar('\n');
+
+return (0);
+
+}
