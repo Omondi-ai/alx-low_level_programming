65231d737f630047f2aafa8469e8b2e150b323aa
Omondi-ai <odhiamboderrick01@gmail.com>
2022-07-03 02:36:09 -0700

    Combinations printing

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main Prints a series of numbers with commas
+ *
+ * Return: Always (Success);
+ */
+
+int main(void)
+{
+	int y;
+
+	for (y = '0'; y <= '9'; y++)
+	{
+		putchar(y);
+
+		if (y != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
