7d29a1460629e6bd002708d631bfba57ae19556d
Omondi-ai <odhiamboderrick01@gmail.com>
2022-07-03 03:00:20 -0700

    Printing combinations of two

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - Prints combination of numbers
+ *
+ * Return: Always (Success)
+ */
+int main(void)
+{
+	int p, i;
+
+	for (p = '0'; c <= '9'; p++)
+	{
+		for (i = '0'; i <= '9'; i++)
+		{
+			if (p < i)
+			{
+				putchar(p);
+				putchar(i);
+
+				if (p != '8' || (p == '8' && i != '9'))
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+
+}
