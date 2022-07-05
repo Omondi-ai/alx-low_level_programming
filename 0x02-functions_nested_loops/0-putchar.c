7830f566f546424120ea9940a0f6a0bf6a1e049a
Omondi-ai <odhiamboderrick01@gmail.com>
2022-07-05 03:08:18 -0700

    testing question 0 for putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <main.h>
+
+/**
+ * main - This program prints putchar
+ *
+ * return (0) is success
+ */
+int main(void)
+{
+	_putchar(char '_', 'p', 'u', 't', 'c', 'h', 'a', 'r', \n);
+	{
+		return (0);
+	}
+	return (0);
+}
