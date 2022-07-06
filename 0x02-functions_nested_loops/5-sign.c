7d5dedeb6dc3fb1ecd6c7736cc453a86237c4cef
Omondi-ai <odhiamboderrickomondi020@gmail.com>
2022-07-06 15:46:47 -0700

    Sign 5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -3,7 +3,7 @@
  * print_sign - prints a number's sign
  * @n: The number to be checked
  *
- * Return:1for positive number,-1 for negative number or zero for anything else
+ * Return:1 for positive num,-1 for negative num or zero for anything else
  */
 int print_sign(int n)
 {
@@ -12,14 +12,14 @@ int print_sign(int n)
 		_putchar('+');
 		return (1);
 	}
-	else if (n < 0)
-	{
-		_putchar('-');
-		return (-1);
-	}
 	else if (n == 0)
 	{
 		_putchar('0');
-		return (0);
+		return ('0');
+	}
+	else if (n < 1)
+	{
+		_putchar('-');
+		return (-1);
 	}
 }
