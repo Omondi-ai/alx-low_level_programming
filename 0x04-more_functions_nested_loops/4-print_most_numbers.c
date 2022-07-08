34d4b8110a5587604700d1b533926098c7503410
Omondi-ai <odhiamboderrickomondi020@gmail.com>
2022-07-08 00:49:14 -0700

    More to be printed

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -3,16 +3,24 @@
 /**
  * print_most_numbers - Print the numbers since 0 up to 9
  *
+ * Description: Prints the numbers excluding 2 and 4
+ *
  * Return: The numbers since 0 to 9
  */
 void print_most_numbers(void)
 {
-	int x;
+	int x = 0;
 
-	for (x = 0; x <= 9; x++)
-
+	for (; x <= 9; x++)
 	{
-		_putchar(x + '0');
+		if (x == 2 || x == 4)
+		{
+			continue;
+		}
+		else
+		{
+			_putchar(x + '0');
+		}
 	}
 	_putchar('\n');
 }
