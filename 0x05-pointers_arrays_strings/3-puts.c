5322c4a16327fe408e61574ddf9b69128350b60d
Omondi-ai <odhiamboderrickomondi020@gmail.com>
2022-07-11 01:46:32 -0700

    3puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _puts - prints a string
+ * @str: the string to be printed
+ *
+ * Description: prints a string
+ * Returns: number of characters printed
+ */
+void _puts(char *str)
+{
+	while (*str)
+		_putchar(*str++);
+		_putchar('\n');
+}
