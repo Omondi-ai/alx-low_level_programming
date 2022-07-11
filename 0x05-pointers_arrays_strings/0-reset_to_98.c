0343a511574c638a4a4e67345fa3cfac7ec358a8
Omondi-ai <odhiamboderrickomondi020@gmail.com>
2022-07-11 01:00:13 -0700

    Resetting to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * reset_to_98 -resets the integer's value to 98
+ * @n:The required parameter
+ * Return: completely nothing
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
