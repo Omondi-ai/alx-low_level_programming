52222257bc1d9a1757681eb16b5ceafaacc5c764
Omondi-ai <odhiamboderrickomondi020@gmail.com>
2022-07-14 04:22:00 -0700

    Strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -7,18 +7,20 @@
  * Return: *dest
  */
 
-int _strcmp(char *s1, charn *s2)
+int _strcmp(char *s1, char *s2)
 {
-	int i;
-	int R;
-
-	i = 0;
-
-	while (s1[i] == s2[i] && (s1[i] != '\0' || s2[i] != '\0'))
+	while ((*s1 != '\0' && *s2 != '\0') && *s1 == *s2)
 	{
-		i++;
+		s1++;
+		s2++;
 	}
-	R = s1[i] - s2[i];
 
-	return (R);
+	if (*s1 == *s2)
+	{
+		return (0);
+	}
+	else
+	{
+		return (*s1 - *s2);
+	}
 }
