b4c363292a58fcf8fb70982952eaa364ba82a9f3
Omondi-ai <odhiamboderrickomondi020@gmail.com>
2022-07-14 02:25:52 -0700

    Strcatcc

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -8,13 +8,10 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	int index = 0, dest_len = 0;
+	char *pointer;
 
-	while (dest[index++])
-		dest_len++;
+	strcat(dest, src);
+	pointer = dest;
 
-	for (index = 0; src[index]; index++)
-		dest[dest_len++] = src[index];
-
-	return (dest);
+	return (pointer);
 }
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,6 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 #include <stdio.h>
+#include <string.h>
 
 int _putchar(char c);
 char *_strcat(char *dest, char *src);
@@ -14,4 +15,4 @@ char *leet(char *);
 char *rot13(char *);
 void print_number(int n);
 
-
+#endif
