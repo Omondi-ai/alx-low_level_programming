b551f1365a4d4065bc6e8594c123d7d6a7dcfb0d
Omondi-ai <odhiamboderrickomondi020@gmail.com>
2022-07-14 04:05:31 -0700

    Cpy str

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strncpy - two words
+ * @dest : pointer to char parameters
+ * @src: pointer to char parameters
+ * @n:int parameters
+ * Return: *dest
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; src[i] != '\0' && i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+
+	for (; i < n; i++)
+	{
+		dest[i] = '\0';
+	}
+
+	return (dest);
+}
