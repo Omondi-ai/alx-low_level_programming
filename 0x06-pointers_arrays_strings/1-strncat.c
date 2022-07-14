8732b269541f0fb6546e64c5d0bf6f04e28661a8
Omondi-ai <odhiamboderrickomondi020@gmail.com>
2022-07-14 03:49:53 -0700

    str

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -9,13 +9,10 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	int index = 0, dest_len = 0;
+	char *pointer;
 
-	while (dest[index++])
-		dest_len++;
-
-	for (index = 0; src[index] && index < n; index++)
-		dest[dest_len++] = src[index];
+	_strncat(dest, src);
+	pointer = dest;
 
 	return (dest);
 }
