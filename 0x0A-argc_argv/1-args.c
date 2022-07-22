430726fc6c3390ea01e7bbabfe873917ad2c0570
Omondi-ai <odhiamboderrickomondi020@gmail.com>
2022-07-22 12:31:42 -0700

    the argsc

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,14 +1,19 @@
-#include "main.h"
 #include <stdio.h>
+#include <stdlib.h>
 /**
  * main - prints all arguments it receives
  * @argc: The number of arguments supplied to program
  * @argv: An array of pointers to the arguments
  * Return: Always 0
  */
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
-	(void) argv;
-	printf("%s\n", argc - 1);
+	int sum;
+
+	for (sum = 0; sum < argc; sum++)
+	{
+		*argv[sum] = *argv[sum];
+	}
+	printf("%d\n", (sum - 1));
 	return (0);
 }
