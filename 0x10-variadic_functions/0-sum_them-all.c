9a11e07aaec16f0efc50f704ce0430ca4d1919ee
Omondi-ai <odhiamboderrickomondi020@gmail.com>
2022-08-04 03:44:29 -0700

    themall

---
+++ 0x10-variadic_functions/0-sum_them-all.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "variadic_functions.h"
+/**
+ * sum_them_all - sums them all
+ * @n: the integer
+ * Return: always 0
+ */
+int sum_them_all(const unsigned int n, ...);
+{
+	int = sum;
+
+	sum = sum_them_all(4, 5, 6, 7);
+	printf("%d\n", sum);
+	sum = sum_them_all(5, 8, 9, 3);
+	printf("%d\n", sum);
+
+	return (0);
+}
